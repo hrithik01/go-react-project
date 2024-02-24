11b88fdc0232c3318b2fd08f81247fb83a3327a6
Hrithik Joshi <40289232+hrithik01@users.noreply.github.com>
2024-02-25 01:05:49 +0530

    Add User struct and implement GetUsers API endpoint

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -29,24 +30,73 @@ func Routers() {
 	}
 }
 
-func GetUsers(wr http.ResponseWriter, req *http.Request) {
-	fmt.Println("GetUsers")
+type User struct {
+	ID          int    `json:"id"`
+	FirstName   string `json:"first_name"`
+	MiddleName  string `json:"middle_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	Gender      string `json:"gender"`
+	CivilStatus string `json:"civil_status"`
+	Birthday    string `json:"birthday"`
+	Contact     string `json:"contact"`
+	Address     string `json:"address"`
 }
+
+func GetUsers(wr http.ResponseWriter, req *http.Request) {
+	wr.Header().Set("Content-Type", "application/json")
+
+	var users []User
+	rows, err := db.Query("SELECT * FROM users")
+
+	if err != nil {
+		log.Fatal(err)
+		http.Error(wr, err.Error(), 500)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.FirstName, &user.MiddleName, &user.LastName, &user.Email, &user.Gender, &user.CivilStatus, &user.Birthday, &user.Contact, &user.Address)
+		if err != nil {
+			log.Fatal(err)
+			http.Error(wr, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, user)
+	}
+	json.NewEncoder(wr).Encode(users)
+}
+
 func CreateUser(wr http.ResponseWriter, req *http.Request) {
 	fmt.Println("CreateUser")
 }
+
 func GetUser(wr http.ResponseWriter, req *http.Request) {
 	fmt.Println("GetUser")
 }
+
 func UpdateUser(wr http.ResponseWriter, req *http.Request) {
 	fmt.Println("UpdateUser")
 }
+
 func DeleteUser(wr http.ResponseWriter, req *http.Request) {
 	fmt.Println("DeleteUser")
 }
 
 func InitDB() {
-	db, err = sql.Open("mysql", "root:root@tcp(app-mysql:3306)/app")
+	// Below is the connection string in format
+	// "username:password@protocol(host:port)/dbname"
+	db, err = sql.Open("mysql", "root@tcp(localhost:3306)/userdb")
+	if err != nil {
+		panic(err.Error())
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to the database")
 }
 
 var db *sql.DB
